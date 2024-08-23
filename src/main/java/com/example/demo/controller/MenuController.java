53693f50736c46d3faf129dbf2f7b8ec80b8f07d
asakunonako <asakuno.nako@dsic.jp>
2024-08-23 17:55:08 +0900

    初期表示

---
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.constant.UrlConst;
+
+/**
+ * メニュー画面 Controller
+ * 
+ * @author asakuno.nako
+ */
+@Controller
+public class MenuController {
+	
+	/**
+	 * 初期表示
+	 * 
+	 * @return 表示画面
+	 */
+	@GetMapping(UrlConst.MENU)
+	public String view() {
+		
+		return "menu";
+	}
+
+}
