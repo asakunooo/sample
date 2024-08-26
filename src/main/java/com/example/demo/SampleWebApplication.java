14aa90dab42b71b9eab095789625dcbbf09e4a97
asakunonako <asakuno.nako@dsic.jp>
2024-08-26 16:02:32 +0900

    SignupServiceインタフェースの作成

--- src/main/java/com/example/demo/SampleWebApplication.java
+++ src/main/java/com/example/demo/SampleWebApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleWebApplication {
 
+	/**
+	 * アプリケーションを起動します。
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SampleWebApplication.class, args);
 	}
